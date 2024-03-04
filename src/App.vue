d57f6ceb176bf08027115f1f36ecf65f5d41f654
Lorenzo Pagani <larrypg2@gmail.com>
2024-03-04 13:07:35 +0100

    working api call

--- src/App.vue
+++ src/App.vue
@@ -13,14 +13,14 @@ export default {
   components: {
     Header,
     Jumbo
-},
+  },
   data() {
     return {
       store,
     };
   },
   mounted() {
-    this.doThings();
+    this.getRestaurants();
 
     // axios.get("indirizzo").then(risultato => {
     // 	console.log(risultato);
@@ -29,17 +29,34 @@ export default {
     // });
   },
   methods: {
-    doThings() {
-      console.log("App.vue does things");
+    getRestaurants() {
+      axios.get("http://localhost:8000/api/restaurants").then(risultato => {
+        console.log(risultato.data);
+        this.store.restaurants = risultato.data;
+      }).catch(errore => {
+        console.error(errore);
+      });
     },
   },
 };
 </script>
 
 <template>
-  <Header/>
-  <Jumbo/>
-  
+  <Header />
+  <Jumbo />
+  <div class="container p-3">
+    <div class="d-flex flex-wrap justify-content-between">
+      <div v-for="restaurants in store.restaurants.data" class="card m-2" style="width:18rem;">
+        <img :src="restaurants.picture" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">{{ restaurants.name }}</h5>
+          <h6 class="card-subtitle mb-2 text-muted ">{{ restaurants.address }}</h6>
+          <p class="card-text">{{ restaurants.description }}</p>
+
+        </div>
+      </div>
+    </div>
+  </div>
   <main></main>
 </template>
 
